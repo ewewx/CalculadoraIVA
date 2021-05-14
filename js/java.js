4c0a7f8fa1573ce6cb509e1d600bad37c6cee5f9
ewewx <77701500+ewewx@users.noreply.github.com>
2021-05-14 14:09:24 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,10 @@
+var numero=prompt("Ingrese el numero para calcular el iva");
+var elemento=document.querySelector(".numero1-numero").innerHTML=numero;
+
+function calculadora_iva(){
+if (numero>0){
+    var iva= numero*0.21;
+    var num_total=(parseInt(numero)+parseInt(iva));
+    var elemento=document.querySelector(".numero2-numero").innerHTML=num_total;
+}
+}
