dce49591e02be79a3f9bfc136272394197d32bb7
ewewx <77701500+ewewx@users.noreply.github.com>
2021-05-15 09:57:52 -0300

    Update java.js

--- js/java.js
+++ js/java.js
@@ -4,7 +4,7 @@ var elemento=document.querySelector(".numero1-numero").innerHTML=numero;
 function calculadora_iva(){
 if (numero>0){
     var iva= numero*0.21;
-    var num_total=(parseInt(numero)+parseInt(iva));
-    var elemento=document.querySelector(".numero2-numero").innerHTML=num_total;
+    var num_total=(parseFloat(numero)+parseFloat(iva));
+    var elemento=document.querySelector(".numero2-numero").innerHTML=num_total.toFixed(4);
 }
 }
